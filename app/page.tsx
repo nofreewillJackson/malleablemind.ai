c0860f4fd26762b76ce5dac16aaad1b26c6d2621
nofreewillJackson <lordryuu@gmail.com>
2024-08-12 19:35:20 +0000

    gfgfg

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
         >
           <h2 className="mb-4 text-3xl font-semibold">
             <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              -&gt;
+              {"->"} 
             </span>
           </h2>
         </a>
@@ -57,7 +57,7 @@ export default function Home() {
         >
           <h2 className="mb-4 text-3xl font-semibold">
             <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              -&gt;
+              {"->"} 
             </span>
           </h2>
         </a>
@@ -70,7 +70,7 @@ export default function Home() {
         >
           <h2 className="mb-4 text-3xl font-semibold">
             <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              -&gt;
+              {"->"} 
             </span>
           </h2>
         </a>
@@ -83,7 +83,7 @@ export default function Home() {
         >
           <h2 className="mb-4 text-3xl font-semibold">
             <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              -&gt;
+              {"->"} 
             </span>
           </h2>
         </a>
