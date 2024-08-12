2d60fb31cf2988d01bd62fcefd8158848c0bdb47
nofreewillJackson <lordryuu@gmail.com>
2024-08-12 20:31:52 +0000

    layot

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,10 @@ export default function RootLayout({
         <meta name="description" content="Jackson & Friends" />
 
         {/* Favicon for most browsers */}
-        <link rel="icon" type="image/png" sizes="32x32" href="app/favicon-32x32.png" /> 
+        <link rel="icon" type="image/png" sizes="32x32" href="/workspaces/malleablemind.ai/app/favicon-32x32.png" /> 
 
         {/* Favicon for Apple devices */}
-        <link rel="apple-touch-icon" sizes="180x180" href="app/apple-touch-icon-152x152.png" /> 
+        <link rel="apple-touch-icon" sizes="180x180" href="/workspaces/malleablemind.ai/app/apple-touch-icon-152x152.png" /> 
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
