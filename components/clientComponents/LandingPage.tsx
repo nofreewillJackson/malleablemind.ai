2259b401f4509bd93b16f083397696092bd21ce8
nofreewillJackson <lordryuu@gmail.com>
2024-08-13 02:54:48 +0000

    hoe

--- components/clientComponents/LandingPage.tsx
+++ components/clientComponents/LandingPage.tsx
@@ -3,89 +3,58 @@ import Image from "next/image";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col items-center justify-center px-4 py-12 sm:p-24">
-      <div className="flex flex-col items-center justify-center">
-        <div className="mb-12">
+    <div className="flex flex-col items-center justify-center px-4 py-12 sm:p-24 w-full max-w-screen-lg mx-auto">
+      <div className="flex flex-col items-center justify-center w-full">
+        <div className="mb-12 w-3/4 sm:w-auto">
           <Image
             src="/Logo.svg"
             alt="Malleable Mind Logo"
             width={600}
             height={144}
             priority
-            className="max-w-full sm:max-w-none"
+            className="w-full"
           />
         </div>
 
-        <div className="definition-container mb-12 text-center sm:text-left" style={{ fontFamily: 'monospace', color: '#4dabf7' }}>
+        <div className="definition-container mb-12 text-center sm:text-left w-2/3 sm:w-auto" style={{ fontFamily: 'monospace', color: '#4dabf7' }}>
           <span className="word" style={{ fontWeight: 'bold' }}>malleable</span> (mal-ee-uh-buhl) <br />
           <span>adj. A canvas for infinite possibilities, forever </span>
           <span id="unfolding" className="unfolding">unfolding</span>
         </div>
 
-        <div className="mb-12">
+        <div className="mb-12 w-3/4 sm:w-auto">
           <Image
             src="/hellscapeLogo.svg"
             alt="Surviving the hellscape of late-stage capitalism"
             width={450}
             height={450}
             priority
-            className="max-w-full sm:max-w-none"
+            className="w-full"
           />
         </div>
       </div>
 
-      <div className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 w-full">
-        <a
-          href="https://malleablemind.ai/excalidraw-notes"
-          className="group rounded-lg border border-transparent px-6 py-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-4 text-xl sm:text-3xl font-semibold">
-            <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              {"->"} 
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="https://malleablemind.ai/learn"
-          className="group rounded-lg border border-transparent px-6 py-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-4 text-xl sm:text-3xl font-semibold">
-            <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              {"->"}  
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="https://malleablemind.ai/templates"
-          className="group rounded-lg border border-transparent px-6 py-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-4 text-xl sm:text-3xl font-semibold">
-            <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              {"->"} 
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="https://malleablemind.ai/deploy"
-          className="group rounded-lg border border-transparent px-6 py-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-4 text-xl sm:text-3xl font-semibold">
-            <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
-              {"->"} 
-            </span>
-          </h2>
-        </a>
+      <div className="mt-12 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 w-full gap-6">
+        {[
+          "https://malleablemind.ai/excalidraw-notes",
+          "https://malleablemind.ai/learn",
+          "https://malleablemind.ai/templates",
+          "https://malleablemind.ai/deploy",
+        ].map((href, index) => (
+          <a
+            key={index}
+            href={href}
+            className="group rounded-lg border border-transparent px-6 py-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="text-xl sm:text-3xl font-semibold flex items-center justify-center">
+              <span className="transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
+                {"->"} 
+              </span>
+            </h2>
+          </a>
+        ))}
       </div>
 
       <style jsx>{`
